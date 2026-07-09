42ce9f8ea6cd20ff2796e02269893eec7b49f35c
Méziane Gostiaux <meziane@gostiaux.fr>
2026-07-09 17:41:47 +0200

    basic pointer movement and cell incrementation and decrement implementation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 #[derive(Debug)]
 pub struct Interpreter {
-    pub raw_code: String,
+    pub code: Vec<char>,
     pub tape: Vec<u8>,
     pub ptr: usize,
     pub step: usize,
@@ -15,7 +15,7 @@ impl fmt::Display for Interpreter {
             "pointer position : {}\ncurrent step : {}\nBF code : '{}'\ntape [..100] : {:?}",
             self.ptr,
             self.step,
-            self.raw_code,
+            self.code.iter().collect::<String>(),
             &(self.tape)[..100],
         )
     }
@@ -24,11 +24,25 @@ impl fmt::Display for Interpreter {
 impl Interpreter {
     pub fn new(raw_code: String, tape_size: usize) -> Self {
         Self {
-            raw_code: raw_code,
+            code: raw_code.chars().collect(),
             tape: vec![0u8; tape_size],
             ptr: 0,
             step: 0,
         }
     }
-    pub fn exec_sbs() {}
+    pub fn exec_sbs(&mut self) {
+        match self.code[self.step] {
+            '>' => self.ptr += 1,
+            '<' => self.ptr -= 1,
+            '+' => self.tape[self.ptr] = self.tape[self.ptr].wrapping_add(1),
+            '-' => self.tape[self.ptr] = self.tape[self.ptr].wrapping_sub(1),
+            _ => {}
+        }
+    }
+    pub fn exec(&mut self) {
+        while self.step < self.code.len() {
+            self.exec_sbs();
+            self.step += 1
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,6 @@ const DEFAULT_TAPE_SIZE: usize = 30_000;
 fn main() {
     let raw_code = fs::read_to_string(FILE_PATH).expect("Error reading the file");
     let mut interp = Interpreter::new(raw_code.trim().to_string(), DEFAULT_TAPE_SIZE);
+    interp.exec();
     println!("{interp}")
 }
