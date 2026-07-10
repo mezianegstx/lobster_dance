a6539810da316d3b8ed5af030d3ed1076e9f9055
Méziane Gostiaux <meziane@gostiaux.fr>
2026-07-10 10:52:47 +0200

    basic loop implementation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,9 @@
-use std::fmt;
+use std::{fmt, thread, time::Duration};
 
 #[derive(Debug)]
 pub struct Interpreter {
     pub code: Vec<char>,
+    pub bracklet_map: Vec<usize>,
     pub tape: Vec<u8>,
     pub ptr: usize,
     pub step: usize,
@@ -23,26 +24,74 @@ impl fmt::Display for Interpreter {
 
 impl Interpreter {
     pub fn new(raw_code: String, tape_size: usize) -> Self {
-        Self {
+        let mut interp = Self {
             code: raw_code.chars().collect(),
+            bracklet_map: vec![0usize; raw_code.len()],
             tape: vec![0u8; tape_size],
             ptr: 0,
             step: 0,
-        }
+        };
+        match interp.build_bracklet_map() {
+            Ok(()) => {}
+            Err(e) => println!("{}", e),
+        };
+        interp
     }
+    fn build_bracklet_map(&mut self) -> Result<(), String> {
+        let mut stack = Vec::new();
+        for (i, char) in self.code.iter().enumerate() {
+            match char {
+                '[' => stack.push(i),
+                ']' => {
+                    let origin = match stack.pop() {
+                        Some(indice) => indice,
+                        None => {
+                            return Err(format!(
+                                "Closed bracklet at position {} without corresponding '['.",
+                                i
+                            ));
+                        }
+                    };
+                    self.bracklet_map[i] = origin;
+                    self.bracklet_map[origin] = i;
+                }
+                _ => {}
+            }
+        }
+        match stack.pop() {
+            Some(indice) => {
+                return Err(format!(
+                    "Opening bracklet found at position {} was never closed.",
+                    indice
+                ));
+            }
+            None => {}
+        }
+        Ok(())
+    }
+
     pub fn exec_sbs(&mut self) {
         match self.code[self.step] {
             '>' => self.ptr += 1,
             '<' => self.ptr -= 1,
             '+' => self.tape[self.ptr] = self.tape[self.ptr].wrapping_add(1),
             '-' => self.tape[self.ptr] = self.tape[self.ptr].wrapping_sub(1),
+            '[' => {
+                if self.tape[self.ptr] == 0 {
+                    self.step = self.bracklet_map[self.step];
+                }
+            }
+            ']' => self.step = self.bracklet_map[self.step] - 1,
             _ => {}
-        }
+        };
+        self.step += 1;
     }
-    pub fn exec(&mut self) {
+    pub fn exec(&mut self, delay_ms: u64) {
+        println!("{:?}", self.bracklet_map);
         while self.step < self.code.len() {
             self.exec_sbs();
-            self.step += 1
+            thread::sleep(Duration::from_millis(delay_ms));
+            println!("{:?}", &(self.tape)[..10])
         }
     }
 }
